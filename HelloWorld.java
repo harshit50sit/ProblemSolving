f1f48f13c5177d28fcb886b3f558f6b4be2266c3
harshit50sit <harshitverma26@gmail.com>
2021-02-14 15:42:12 +0530

    Scanner

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class HelloWorld
+{
+    public static void main(String ss[])
+    {
+        System.out.println("harshit");
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        System.out.println(a);
+    }
+}

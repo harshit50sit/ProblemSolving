b48ab17458fd366ab677d2e554d29f25862c02b0
harshit50sit <harshitverma26@gmail.com>
2021-02-14 16:06:16 +0530

    Replace_the_Variable

---
+++ String_Problems/ReplaceCharacters.java
@@ -0,0 +1,11 @@
+package String_Problems;
+import java.util.*;
+public class ReplaceCharacters {
+    public static void main(String ss[])
+    {
+        String str = "3242356";
+        str = str.replace("56", "");
+        System.out.println(str);
+    }
+    
+}

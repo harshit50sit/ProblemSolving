52cb65cb6f071f087a3fd1f2028b9a10f11f2f97
harshit50sit <harshitverma26@gmail.com>
2021-03-19 11:37:29 +0530

    Reversing_a_linked_lidt

---
+++ Data_Structure_and_algorithms_Using_Java/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,64 @@
+import java.util.*;
+public class ReverseLinkedList {
+    Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    public void insertData(int data)
+    {
+        Node new_node = new Node(data);
+        if(head == null)
+        {
+            head = new Node(data);
+            return;
+        }
+        new_node.next = null;
+        Node last = head;
+        while(last.next != null)
+        {
+            last = last.next;
+        }
+        last.next = new_node;
+    }
+    public void printList()
+    {
+        Node n = head;
+        while(n != null)
+        {
+            System.out.print(n.data+"---->");
+            n = n.next;
+        }
+    }
+    public static void main(String ss[])
+    {
+        ReverseLinkedList l1 = new ReverseLinkedList();
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println();
+            System.out.println("1-Insert a data at end"+"\n"+"2-Reverse a linked list"+"\n"+"3-To print the data"+"\n"+"4-Exit");
+            System.out.println("Enter choice");
+            int ch = sc.nextInt();
+            switch(ch)
+            {
+                case 1:System.out.println("Enter data");
+                       int d = sc.nextInt();
+                       l1.insertData(d);
+                       break;
+                case 2:System.out.println("Reversed Linked List");
+                       break;
+                case 3:l1.printList();
+                       break;
+                case 4:System.exit(0);
+                default:System.out.println("Invalid choice");
+            }
+        }
+    }
+}

2da7f83e0cb141885282f2d90104be8b6a811449
harshit50sit <harshitverma26@gmail.com>
2021-03-24 21:02:34 +0530

    Postfix_evaulation

--- Data_Structure_and_algorithms_Using_Java/Stack/PostfixEvaulation.java
+++ Data_Structure_and_algorithms_Using_Java/Stack/PostfixEvaulation.java
@@ -1,3 +1,61 @@
+//Implementation of Postfix evaluation using stack
+//Input-123*+5-
+//Output-2
+//Input-231*+9-
+//Output= -4
+import java.util.*;
 public class PostfixEvaulation {
-    
+    public static int expressionEvaluation(String tokens[])
+    {
+        /*for(String i : tokens)
+        {
+            System.out.println(i);
+        }*/
+        Stack<Integer> s = new Stack<Integer>();
+        for(String token : tokens)
+        {
+            if(token.equals("+"))
+            {
+                int op1 = s.pop();
+                int op2 = s.pop();
+                int res = op1 + op2;
+                s.push(res);
+            }
+            else if(token.equals("-"))
+            {
+                int op1 = s.pop();
+                int op2 = s.pop();
+                int res = op2 - op1;
+                s.push(res);
+            }
+            else if(token.equals("*"))
+            {
+                int op1 = s.pop();
+                int op2 = s.pop();
+                int res = op2 * op1;
+                s.push(res);
+            }
+            else if(token.equals("/"))
+            {
+                int op1 = s.pop();
+                int op2 = s.pop();
+                int res = op2 / op1;
+                s.push(res);
+            }
+            else
+            {
+                s.push(Integer.parseInt(token));
+            }
+        }
+        return s.pop();
+    }
+    public static void main(String ss[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a expression");
+        String exp = sc.next();
+        //String tokens[] = exp.split("");
+        System.out.println(tokens.length);
+        System.out.println(expressionEvaluation(tokens));
+    }
 }

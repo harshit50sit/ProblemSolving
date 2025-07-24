ef02c0723341fc0d6dec769b00163898bcf394ec
harshit50sit <harshitverma26@gmail.com>
2025-07-24 14:48:47 +0530

    GS_Interview_Question_rename

--- String_Problems/MaxAverageStudent.java
+++ String_Problems/MaxAverageStudent.java
@@ -2,7 +2,7 @@ package String_Problems;
 
 
 import java.util.*;
-public class Solution {
+public class MaxAverageStudent {
     public static void main(String[] args) {
        String[][] scores = {
               {"jerry", "75"},
